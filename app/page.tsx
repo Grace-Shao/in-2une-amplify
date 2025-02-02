94fe80c03ed815208e13944397c8f867a367d278
Grace-Shao <gshao8@gatech.edu>
2025-02-02 10:00:19 -0500

    combined landing file

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import Link from "next/link";
+import Image from "next/image";
 
 Amplify.configure(outputs);
 
@@ -32,21 +34,45 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+    <div>
+      <video
+        autoPlay
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/arcadeVideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="flex flex-col items-center justify-center min-h-screen p-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col items-center gap-8">
+          <header className="absolute top-4 mt-4">
+            <Image
+              src="/logoCrop.png"
+              alt="Your Logo"
+              width={360}
+              height={120}
+              priority
+            />
+          </header>
+          <Link href="/userTypePage">
+            <button className="absolute inset-0 w-full h-full bg-transparent text-white text-4xl font-bold flex items-center justify-center"></button>
+          </Link>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+        </main>
       </div>
-    </main>
+    </div>
   );
 }

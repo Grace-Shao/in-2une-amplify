dd0750c07b4dc2fa97ed47aa0176c0eaf934bbe8
Grace-Shao <gshao8@gatech.edu>
2025-02-02 10:12:32 -0500

    update main landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,48 +5,44 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json"; // Adjusted path
 import "@aws-amplify/ui-react/styles.css";
+import Link from "next/link";
+import Image from "next/image";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+  // Removed todo list state and functions
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+    <div>
+      <video
+        autoPlay
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/arcadeLandingPage.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="flex flex-col items-center justify-center min-h-screen p-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col items-center gap-8">
+          <header className="absolute top-4 mt-4">
+            <Image
+              src="/logoCrop.png"
+              alt="Your Logo"
+              width={360}
+              height={120}
+              priority
+            />
+          </header>
+          <Link href="/userTypePage">
+            <button className="absolute inset-0 w-full h-full bg-transparent text-white text-4xl font-bold flex items-center justify-center"></button>
+          </Link>
+        </main>
       </div>
-    </main>
+    </div>
   );
 }
